f287cd1720f9ea290d56023abef8196879465347
KolesnikovMN <Mikael-28@yandex.ru>
2021-01-16 16:54:43 +0300

    finall version

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,16 @@
-const slider = document.querySelector('.swiper-container')
+const slider = document.querySelector('.swiper-container');
+const slider1 = document.querySelector('.swiper-container1');
+
 var mySwiper = new Swiper(slider, {
             // Optional parameters
+            autoplay: {
+                delay: 5000,
+            },
             loop: true,
 
             // If we need pagination
             pagination: {
-                el: '.swiper-pagination',
+                el: '.pag-1',
             },
 
             // Navigation arrows
@@ -13,4 +18,4 @@ var mySwiper = new Swiper(slider, {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
             },
-        })
+        });
